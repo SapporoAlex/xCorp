165acff2b4f2473121303fa7e729afbf885aec40
Alex_McKinley <alexanderfromaustralia@gmail.com>
2025-10-17 23:45:12 +0900

    nc

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,11 +27,11 @@ const Header: React.FC = () => {
         <h1 className='header-logo'>X Corp.</h1>
         <div className="nav-bar-right">
           <nav className="u-pc-only">
-            <Link to="/" onClick={handleTopClick}>{t('header.top')}</Link> |{' '}
-            <HLink smooth to="/#about-section">{t('header.about')}</HLink> |{' '}
-            <Link to="/staff">{t('header.staff')}</Link> |{' '}
-            <HLink smooth to="/#media-section">{t('header.media')}</HLink> |{' '}
-            <HLink smooth to="/#contact-section">{t('header.contact')}</HLink>
+            <Link to="/xCorp" onClick={handleTopClick}>{t('header.top')}</Link> |{' '}
+            <HLink smooth to="/xCorp/#about-section">{t('header.about')}</HLink> |{' '}
+            <Link to="/xCorp/staff">{t('header.staff')}</Link> |{' '}
+            <HLink smooth to="/xCorp/#media-section">{t('header.media')}</HLink> |{' '}
+            <HLink smooth to="/xCorp/#contact-section">{t('header.contact')}</HLink>
           </nav>
           {/* Mobile Navigation */}
           <nav className="u-sp-only">
@@ -46,10 +46,10 @@ const Header: React.FC = () => {
             {isOpen && (
               <div className="mobile-menu">
                 <Link to="/" onClick={handleTopClick}>{t('header.top')}</Link>
-                <HLink smooth to="/#about-section" onClick={() => setIsOpen(false)}>{t('header.about')}</HLink>
-                <Link to="/staff" onClick={() => setIsOpen(false)}>{t('header.staff')}</Link>
-                <HLink smooth to="/#media-section" onClick={() => setIsOpen(false)}>{t('header.media')}</HLink>
-                <HLink smooth to="/#contact-section" onClick={() => setIsOpen(false)}>{t('header.contact')}</HLink>
+                <HLink smooth to="/xCorp/#about-section" onClick={() => setIsOpen(false)}>{t('header.about')}</HLink>
+                <Link to="/xCorp/staff" onClick={() => setIsOpen(false)}>{t('header.staff')}</Link>
+                <HLink smooth to="/xCorp/#media-section" onClick={() => setIsOpen(false)}>{t('header.media')}</HLink>
+                <HLink smooth to="/xCorp/#contact-section" onClick={() => setIsOpen(false)}>{t('header.contact')}</HLink>
               </div>
             )}
             </nav>
